f15413acd400ba466778405b3eba44d8baa2653b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-20 09:35:30 -0800

    function generator called

---
+++ project 1/three.js
@@ -0,0 +1,14 @@
+function* forever(){
+    let index = 30;
+    while(true){
+        yield index++;
+    }
+}
+
+let f = forever();
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
