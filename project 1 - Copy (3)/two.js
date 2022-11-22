39f5e180ebab045f90593bc95dd48d111bbf0c76
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-22 09:34:07 -0800

    generator function called

---
+++ project 1 - Copy (3)/two.js
@@ -0,0 +1,13 @@
+function* forever() {
+    let index = 20;
+    while (true) {
+        yield index++
+    }
+}
+
+let f = forever();
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
