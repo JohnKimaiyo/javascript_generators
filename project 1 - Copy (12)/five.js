6a82edc808e5b1770e87f28e0136d0fa544f453a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-23 01:46:54 -0800

    generaton function called

---
+++ project 1 - Copy (12)/five.js
@@ -0,0 +1,14 @@
+function* money() {
+    let index = 50;
+    while (true) {
+        yield index++
+    }
+}
+
+let m = money();
+
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
