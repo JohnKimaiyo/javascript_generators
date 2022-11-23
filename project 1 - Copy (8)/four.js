41d19e40f48f59ccc51492dbb8a3b29c458e3942
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-22 22:49:35 -0800

    genrator function called

---
+++ project 1 - Copy (8)/four.js
@@ -0,0 +1,14 @@
+function* forever(){
+    let index = 40;
+    while(true){
+        yield index++
+    }
+}
+
+let f = forever();
+
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
