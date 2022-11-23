493ae22d2d36375bd6b360b52474a1a087e5ee28
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-22 22:44:53 -0800

    generator function called

---
+++ project 1 - Copy (8)/three.js
@@ -0,0 +1,13 @@
+function* forever(){
+    let index = 30;
+    while(true){
+        yield index ++
+    }
+}
+
+let f = forever();
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
