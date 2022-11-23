e493ba3d943221898f2e47703f13b83780a7bb0e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-23 04:10:04 -0800

    functon generator

---
+++ project 1 - Copy (14)/five.js
@@ -0,0 +1,13 @@
+function* money() {
+    let index = 50;
+    while (true) {
+        yield index++
+    }
+}
+
+let m = money();
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
