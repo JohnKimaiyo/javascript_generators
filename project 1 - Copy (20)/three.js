b0fc0e73e5c58b56ee549e1188c259962afeaa5c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-23 17:24:40 -0800

    function generator called

---
+++ project 1 - Copy (20)/three.js
@@ -0,0 +1,13 @@
+function* money() {
+    let index = 30;
+    while (true) {
+        yield index++
+    }
+}
+
+let m = money();
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
