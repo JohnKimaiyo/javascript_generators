039fc9a08bdb77c001967955652cdbf2715c0343
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-23 16:51:13 -0800

    function generator

---
+++ project 1 - Copy (19)/two.js
@@ -0,0 +1,13 @@
+function* money() {
+    let index = 20;
+    while (true) {
+        yield index++
+    }
+}
+
+let m = money();
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
