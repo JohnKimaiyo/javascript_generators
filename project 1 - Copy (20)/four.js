84b1c67cf2877e7bfecdb4ad6b5984c086180e49
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-23 17:28:18 -0800

    function generator called

---
+++ project 1 - Copy (20)/four.js
@@ -0,0 +1,14 @@
+function* money(){
+    let index = 40;
+    while(true){
+        yield index++
+    }
+}
+
+let m = money();
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+console.log(m.next());
+
